14987479121cccc1ffdbc0e9ed2690f0349e4a70
remerjohnson <ryan.emerson.johnson@gmail.com>
2015-11-10 12:57:55 -0800

    Initial commit

---
+++ FAST_times.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+from operator import itemgetter
+import urllib
+import requests
+import csv
+import re
+
+#some config
+api_base_url = 'http://fast.oclc.org/searchfast/fastsuggest'
+#For constructing links to FAST.
+fast_uri_base = 'http://id.worldcat.org/fast/{0}'
+
+# Open the file 
+f1 = open('geographic.csv', 'rb')
+
+# Set up to write to a csv file
+# f2 = 
+
+# Create subjects list we can append all the subjects from column B to:
+subjects = []
+for line in f1:
+	subj = re.findall("\t(.+)\t", line, flags=re.UNICODE)
+	subjects.append(subj)
+	continue
+
+print json.dumps(subjects, ensure_ascii=False)
+
